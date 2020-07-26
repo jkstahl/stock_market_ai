41bbe6d9a2f8c339ba6c3d10984ff5ac316c7dc9
jkstahl <neobaud@gmail.com>
2020-07-25 19:37:32 -0500

    Looking good

--- stock_price.py
+++ stock_price.py
@@ -1,3 +1,16 @@
-class StockPrice():
-    def __init__(self):
-        pass
+import yfinance as yf
+import pandas as pd
+from data_storage import GenericSeriesStorage
+
+class StockPrice(GenericSeriesStorage):
+    
+    def get_new_data(self, items, start, end):
+        return yf.download(items, start = start, end = end)['Adj Close']
+    
+
+
+
+if __name__ ==  '__main__':
+    sp = StockPrice()
+    print (sp.get_stock_price(['MSFT', 'INTC'], "2017-01-01", "2017-04-30"))
+    print (sp.get_stock_price(['MSFT', 'INTC'], "2017-01-02", "2017-04-29"))
